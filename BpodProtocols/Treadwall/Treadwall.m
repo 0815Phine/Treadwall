f3f313bdcb0a0bdd93bc4db28795c2d74200f77d
Josephine Timm <117468201+0815Phine@users.noreply.github.com>
2024-05-28 14:09:08 +0200

    adjusted triallist loading

--- BpodProtocols/Treadwall/Treadwall.m
+++ BpodProtocols/Treadwall/Treadwall.m
@@ -6,8 +6,9 @@ global BpodSystem
 % initialize parameters
 S = BpodSystem.ProtocolSettings;
 
-params_file = '';
-run(params_file)
+%params_file = '';
+%run(params_file)
+start_path = 'Z:\Animals\Cohort00_Test'; %check on experiment computer
 
 if isempty(fieldnames(S))
     freshGUI = 1;        %flag to indicate that prameters have not been loaded from previous session.
@@ -18,7 +19,7 @@ if isempty(fieldnames(S))
     S.GUI.ITIDur = 12; %in seconds
     S.GUI.stimDur = 60; %in seconds
 
-    S.GUI.ExpInfoPath = 'Z:\Animals\Cohort00_Test\#Test';
+    S.GUI.ExpInfoPath = start_path;
 else
     freshGUI  = 0;        %flag to indicate that prameters have been loaded from previous session.
 end
@@ -26,15 +27,16 @@ end
 BpodParameterGUI('init', S);
 
 % define and randomize trials
-trialList = dir([S.GUI.ExpInfoPath '*' S.GUI.SubjectID '*' S.GUI.SessionID '.csv']);
-if isempty(trialList), expinfo_trial_name = uigetfile();
-else, expinfo_trial_name = fullfile(trialList.folder, trialList.name);
+% trialList_Info = dir([start_path '\' '#Test' '\' '01' '\' 'triallist' '.csv'])
+trialList_Info = dir([S.GUI.ExpInfoPath '\' S.GUI.SubjectID '\' S.GUI.SessionID '\triallist.csv']);
+if isempty(trialList_Info)
+    [~,triallist_dir] = uigetfile(fullfile(start_path,'*.csv'));
+else
+    triallist_dir = fullfile(trialList_Info.folder, trialList_Info.name);
 end
 
-trialInfo = readtable(expinfo_trial_name);
-seqTrials = trialInfo.Type;
-typeList = trialInfo.Stimulus;
-S.GUI.MaxTrialNumber = numel(seqTrials);
+triallist = readtable(triallist_dir);
+S.GUI.MaxTrialNumber = numel(triallist);
 
 %% ---------- Analog Output Module ----------------------------------------
 BpodSystem.assertModule('WavePlayer1', {'WavePlayer1'});
@@ -63,7 +65,7 @@ for currentTrial = 1:S.GUI.MaxTrialNumber
     S = BpodParameterGUI('sync', S); %Sync parameters with BpodParameterGUI plugin
 
     % read output action
-    switch typeList{currentTrial}
+    switch triallist{currentTrial}
         case 'C'
             fixedVoltage = 5;
         case 'L'
