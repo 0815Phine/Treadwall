d111d089c88a6d16b1c81a4d76de49c9452ea886
Josephine Timm <117468201+0815Phine@users.noreply.github.com>
2024-11-04 17:08:29 +0100

    Update singleSensor.ino

--- Code/Arduino/Distance_Sensor/singleSensor/singleSensor.ino
+++ Code/Arduino/Distance_Sensor/singleSensor/singleSensor.ino
@@ -1,17 +1,13 @@
 //    Arduino pins:
-#define sensorPin A1  // VOut optical sensor 
-#define ledPin 10     // pulse optical sensor 
+#define sensorPin A0  // VOut optical sensor 
+#define ledPin 11     // pulse optical sensor 
 
 // Variables
-int sensValR = 0;
-int sensValOnR = 0;
-int sensValOffR = 0;
-int sensValL = 0;
-int sensValOnL = 0;
-int sensValOffL = 0;
+int sensVal = 0;
+int sensValOn = 0;
+int sensValOff = 0;
 int buffersensVal = 0;
-float sensVoltageR = 0;
-float sensVoltageL = 0;
+float sensVoltage = 0;
 
 
 void PulseTrain(){
@@ -19,26 +15,22 @@ void PulseTrain(){
   digitalWrite(ledPin, HIGH);
   delay(0.4);
   // read the value from the sensor:
-  sensorValueOn = analogRead(sensorPin);
+  sensValOn = analogRead(sensorPin);
   delay(0.4);
   // turn the ledPin off:
   digitalWrite(ledPin, LOW);
   delay(0.4);
   // read the value from the sensor:
-  sensorValueOff = analogRead(sensorPin);
+  sensValOff = analogRead(sensorPin);
   delay(0.8);
 }
 
 void StreamData() {
-  sensValR = sensValOnR - sensValOffR;
-  sensVoltageR = sensValR * (5.0 / 1023.0)*1000; //in mV
-  sensValL = sensValOnL - sensValOffL;
-  sensVoltageL = sensValL * (5.0 / 1023.0)*1000; //in mV
+  sensVal = sensValOn - sensValOff;
+  sensVoltage = sensVal * (5.0 / 1023.0)*1000; //in mV
 
   // Send measurements to Serial in CSV format
-  Serial.print(sensVoltageR);
-  Serial.print(",");
-  Serial.println(sensVoltageL);
+  Serial.println(sensVoltage);
 }
 
 
