70b52884a0fd257e8f5d91aea6224cc38f0150c6
Josephine Timm <117468201+0815Phine@users.noreply.github.com>
2024-12-06 15:16:25 +0100

    Update Distance_Sensor.ino
    adjust tuning curve values and added interpolation of distance. pwmStream is still not properly displaying the distance and values have to be adjust to final tuning curve

--- Code/Arduino/Distance_Sensor/Distance_Sensor/Distance_Sensor.ino
+++ Code/Arduino/Distance_Sensor/Distance_Sensor/Distance_Sensor.ino
@@ -4,15 +4,13 @@
 #define ledPinright 11     // pulse optical sensor right
 #define sensorPinleft A1   // VOut optical sensor left
 #define ledPinleft 10      // pulse optical sensor left
-#define DataStreamright 12 // PWM pin 
-#define DataStreamleft 13  // PWm pin
-//    the following constants have to be adjusted to the final tuning curve
-#define MinVoltage 0 //??
-#define MaxVoltage_left 762.46 // in mV
-#define MaxVoltage_right 3235.58 // in mV
+#define DataStreamright 13 // PWM pin 
+#define DataStreamleft 12  // PWm pin
+//    
 #define MinDistance_left 11 // in mm
-#define MinDistance_right 9.5 // in mm
-#define MaxDistance 32.5 // in mm ??
+#define MinDistance_right 10 // in mm
+#define MaxDistance_left 32 // in mm
+#define MaxDistance_right 31 // in mm 
 #define MaxPWMValue 255 // value to generate 5V with PWM
 #define pwmBaseline 127
 
@@ -30,8 +28,30 @@ float sensVoltageL = 0;
 volatile static float Distance_right = 0.00;
 volatile static float Distance_left = 0.00;
 
+// Constants and Variables from Tuning Curve. Currently have to be hardcoded her (in increasing order of x_axis = voltage).
+// Alternative: Connect SD card with CSV-File to Arduino and read directly from there
+// had to remove the 'baseline' voltage value as this would interfere with the interpolation
+#define NUM_POINTS 8
+float distances_left[NUM_POINTS] = {11, 14, 17, 20, 23, 26, 29, 32};  // Distances in mm
+float voltages_left[NUM_POINTS] = {2923.99, 3026.64, 3103.62, 3173.27, 3227.03, 3233.75, 3241.69, 3242.94};  // Average voltages (mV)
+float distances_right[NUM_POINTS] = {10, 13, 16, 19, 22, 25, 28, 31};  // Distances in mm
+float voltages_right[NUM_POINTS] = {481.43, 540.08, 572.46, 593.85, 636.6, 676.92, 713.59, 733.14};  // Average voltages (mV)
+
 float mapfloat(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x-in_min) * (out_max-out_min) / (in_max - in_min) + out_min;
+}
+
+float interpolate(float x, float* x_points, float* y_points, int num_points) {
+  if (x <= x_points[0]) return y_points[0];  // Below range
+  if (x >= x_points[num_points - 1]) return y_points[num_points - 1]; // Above range
+  
+  for (int i = 0; i < num_points - 1; i++) {
+    if (x >= x_points[i] && x <= x_points[i + 1]) {
+      // Linear interpolation
+      return y_points[i] + (x - x_points[i]) * (y_points[i + 1] - y_points[i]) / (x_points[i + 1] - x_points[i]);
+    }
+  }
+  return y_points[0];  // Fallback (shouldn't reach here)
 }
 
 void PulseTrain(){
@@ -73,28 +93,31 @@ void StreamData() {
   sensVoltageR = sensValR * (5.0 / 1023.0)*1000;
   sensValL = sensValOnL - sensValOffL;
   sensVoltageL = sensValL * (5.0 / 1023.0)*1000;
-  
+
+  Distance_right = interpolate(sensVoltageR, voltages_right, distances_right, NUM_POINTS);
+  Distance_left = interpolate(sensVoltageL, voltages_left, distances_left, NUM_POINTS);
+
   // Analog Stream:
-  //pwmOutput = mapfloat(sensVoltageR, MinVoltage, MaxVoltage, 0, MaxPWMValue);
-  //pwmOutput = constrain(pwmOutput, 0, MaxPWMValue);
-  //analogWrite(DataStreamright, pwmOutput);
+  pwmOutput = mapfloat(Distance_right, MinDistance_right, MaxDistance_right, 0, MaxPWMValue);
+  pwmOutput = constrain(pwmOutput, 0, MaxPWMValue);
+  analogWrite(DataStreamright, pwmOutput);
 
-  //pwmOutput = mapfloat(sensVoltageL, MinVoltage, MaxVoltage, 0, MaxPWMValue);
-  //pwmOutput = constrain(pwmOutput, 0, MaxPWMValue);
-  //analogWrite(DataStreamleft, pwmOutput);
+  pwmOutput = mapfloat(Distance_left, MinDistance_left, MaxDistance_left, 0, MaxPWMValue);
+  pwmOutput = constrain(pwmOutput, 0, MaxPWMValue);
+  analogWrite(DataStreamleft, pwmOutput);
+}
 
-  // Seriel Stream:
-  Distance_right = mapfloat(sensVoltageR, MinVoltage, MaxVoltage, MinDistance_right, MaxDistance);
-  //Serial.print("sensor = ");
-  //Serial.println(sensorVoltage);
+void SerialStream() {
+  Serial.print("sensor right= ");
+  Serial.println(sensVoltageR);
   Serial.print("Distance in mm right Side = ");
   Serial.println(Distance_right);
 
-  Distance_left = mapfloat(sensVoltageL, MinVoltage, MaxVoltage, MinDistance_left, MaxDistance);
-  //Serial.print("sensor = ");
-  //Serial.println(sensorVoltage);
+  Serial.print("sensor left= ");
+  Serial.println(sensVoltageL);
   Serial.print("Distance in mm left Side = ");
   Serial.println(Distance_left);
+  delay(1000);
 }
 
 
@@ -107,4 +130,5 @@ void setup() {
 void loop() {
   PulseTrain();
   StreamData();
+  //SerialStream();
 }
