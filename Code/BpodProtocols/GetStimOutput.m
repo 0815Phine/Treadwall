22df06dcb8cf7f5731f2b8f5afa55ff0c262b020
Josephine Timm <117468201+0815Phine@users.noreply.github.com>
2025-02-13 14:28:04 +0100

    create new Stimulus outputs
    -> direct mapping on distance

--- Code/BpodProtocols/GetStimOutput.m
+++ Code/BpodProtocols/GetStimOutput.m
@@ -2,29 +2,29 @@
 function stimOutput = GetStimOutput(trialType)
     switch trialType
         case 'C45'
-            stimOutput = ['P' 1 0];
+            stimOutput = ['!' 3 18 130];
         case 'C39'
-            stimOutput = ['P' 1 1];
+            stimOutput = ['!' 3  34 155];
         case 'C33'
-            stimOutput = ['P' 1 2];
+            stimOutput = ['!' 3 181 194];
         case 'C27'
-            stimOutput = ['P' 1 3];
+            stimOutput = ['!' 3 205 210];
         case 'L45'
-            stimOutput = ['P' 1 4];
+            stimOutput = ['>' 1 5 255 255];
         case 'L39'
-            stimOutput = ['P' 1 5];
+            stimOutput = ['>' 2 6 255 255];
         case 'L33'
-            stimOutput = ['P' 1 6];
+            stimOutput = ['>' 3 7 255 255];
         case 'L27'
-            stimOutput = ['P' 1 7];
+            stimOutput = ['>' 4 8 255 255];
         case 'R45'
-            stimOutput = ['P' 1 8];
+            stimOutput = ['>' 5 1 255 255];
         case 'R39'
-            stimOutput = ['P' 1 9];
+            stimOutput = ['>' 6 2 255 255];
         case 'R33'
-            stimOutput = ['P' 1 10];
+            stimOutput = ['>' 7 3 255 255];
         case 'R27'
-            stimOutput = ['P' 1 11];
+            stimOutput = ['>' 8 4 255 255];
         otherwise
             error('Unknown trial type: %s', trialType);
     end
