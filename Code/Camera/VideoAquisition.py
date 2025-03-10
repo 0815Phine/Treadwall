d1ace4f9342deada569255e25d06fb8457cbbd56
Josephine Timm <117468201+0815Phine@users.noreply.github.com>
2025-03-10 11:10:17 +0100

    include writing

--- Code/Camera/VideoAquisition.py
+++ Code/Camera/VideoAquisition.py
@@ -27,15 +27,40 @@ cam.TriggerActivation.Value = "RisingEdge"
 cam.TriggerSelector.Value = "FrameStart"
 
 # video aquisition
+cam.StartGrabbing(py.GrabStrategy_OneByOne)
+
+# set output directory
+op_name = 'output.avi'
+
+# set up video writer
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-video_writer = cv2.VideoWriter('output.avi', fourcc, fps=200, frameSize=(1440, 1080))
+video_writer = cv2.VideoWriter(op_name, fourcc, fps=200, frameSize=(1440, 1080), isColor=False)
 
-cam.StartGrabbingMax(400)
+# 
+fcount = 0
+f_failed = 0
+flag_status = False
 
-while cam.IsGrabbing():
-    res = cam.RetrieveResult(1000)
-    image = cv2.cvtColor(res.Array, cv2.COLOR_GRAY2BGR)
-    video_writer.write(image)
-    res.Release()
-cam.Close()
-video_writer.release()
+while not flag_status: # while line status is false check line status again (waiting for first trigger)
+    flag_status = cam.LineStatus.Value
+    if flag_status: # if true: run pipeline
+        stime = time.time()
+        while cam.IsGrabbing():
+            res = cam.RetrieveResult(1000)
+            if res.GrabSucceeded():
+                fcount += 1
+                image = cv2.cvtColor(res.Array, cv2.COLOR_GRAY2BGR)
+                video_writer.write(image)
+            else:
+                f_failed += 1
+            res.Release()
+        
+        # to include: mechanism of stopping pipeline
+        cam.StopGrabbing()
+        cam.Close()
+        video_writer.release()
+        break
+
+fcount
+f_failed
+# to include: time after stopping (to check for correct framerate)
