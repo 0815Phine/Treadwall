ff3ecaeca14c9c36588a490e8ac65a1fde72290f
Josephine Timm <117468201+0815Phine@users.noreply.github.com>
2025-05-14 17:56:02 +0200

    updated for 0-indexing

--- Code/BpodProtocols/HelperFunctions/GetStimOutput.m
+++ Code/BpodProtocols/HelperFunctions/GetStimOutput.m
@@ -2,31 +2,31 @@
 function stimOutput = GetStimOutput(trialType)
     switch trialType
         case 'C51'
-            stimOutput = ['>' 9 3 255 255];
+            stimOutput = ['>' 8 2 255 255];
         case 'C45'
-            stimOutput = ['>' 10 4 255 255]; %['!' 3 18 130];
+            stimOutput = ['>' 9 3 255 255]; %['!' 3 18 130];
         case 'C39'
-            stimOutput = ['>' 11 5 255 255]; %['!' 3  34 155];
+            stimOutput = ['>' 10 4 255 255]; %['!' 3  34 155];
         case 'C33'
-            stimOutput = ['>' 12 6 255 255]; %['!' 3 181 194];
+            stimOutput = ['>' 11 5 255 255]; %['!' 3 181 194];
         case 'C27'
-            stimOutput = ['>' 13 13 255 255]; %['!' 3 205 210];
+            stimOutput = ['>' 12 12 255 255]; %['!' 3 205 210];
         case 'L51'
-            stimOutput = ['>' 7 5 255 255];
+            stimOutput = ['>' 6 4 255 255];
         case 'L45'
-            stimOutput = ['>' 8 6 255 255];
+            stimOutput = ['>' 7 5 255 255];
         case 'L39'
-            stimOutput = ['>' 9 7 255 255];
+            stimOutput = ['>' 8 6 255 255];
         % case 'L33'
         %     stimOutput = ['>' 3 7 255 255];
         % case 'L27'
         %     stimOutput = ['>' 4 8 255 255];
         case 'R51'
-            stimOutput = ['>' 11 1 255 255];
+            stimOutput = ['>' 10 0 255 255];
         case 'R45'
-            stimOutput = ['>' 12 2 255 255];
+            stimOutput = ['>' 11 1 255 255];
         case 'R39'
-            stimOutput = ['>' 13 3 255 255];
+            stimOutput = ['>' 12 2 255 255];
         % case 'R33'
         %     stimOutput = ['>' 7 3 255 255];
         % case 'R27'
