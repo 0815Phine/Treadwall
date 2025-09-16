7c97bd8abedfd9c70934ba326c43f189591438bc
Josephine Timm <117468201+0815Phine@users.noreply.github.com>
2025-09-16 17:14:03 +0200

    shorten

--- Code/BpodProtocols/treadwall_scrambled_parameters_BN.m
+++ Code/BpodProtocols/treadwall_scrambled_parameters_BN.m
@@ -1,6 +1,6 @@
 %% Parameters for Treadwall Experiments
 
 % Timings
-ITIDur = 15; %in seconds
-stimDur = 60; %in seconds
+ITIDur = 10; %in seconds
+stimDur = 50; %in seconds
 waveforms = {1.5, 2.1, 2.6, 3.2, 3.8, 4.2, 5};
