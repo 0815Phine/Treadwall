67870dc9eb2ba29ad15af4436ae941d156d20b7d
Josephine Timm <117468201+0815Phine@users.noreply.github.com>
2026-08-21 13:49:31 +0200

    rename and move
    - parameters all capital letters
    - parameters in separated folder

---
+++ Code/BpodProtocols/parameters/treadwall_h1_parameters.m
@@ -0,0 +1,8 @@
+%% Parameters for Treadwall Experiments
+% for habituation 1 (longer stimulus duration)
+
+% Timings
+STIM_DUR = 200; %in seconds
+ITI_DUR = 15; %in seconds
+
+WAVEFORMS = {0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5};
---
+++ Code/BpodProtocols/parameters/treadwall_sc_p_parameters.m
@@ -0,0 +1,8 @@
+%% Parameters for Treadwall Experiments
+% for scrambled and predictable
+
+% Timings
+ITI_DUR = 10; % in seconds
+STIM_DUR = 50; % in seconds
+
+WAVEFORMS = {1.5, 2.1, 2.6, 3.2, 3.8, 4.2, 5};
